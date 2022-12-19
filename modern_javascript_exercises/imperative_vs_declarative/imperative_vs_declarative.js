a36c54556836094788906f18b0b665cfe0324323
jeyanthi-sm <111299753+jeyanthi-sm@users.noreply.github.com>
2022-12-19 21:14:40 +0000

    updated the function to be declarative rather than a imperative statement

--- modern_javascript_exercises/imperative_vs_declarative/imperative_vs_declarative.js
+++ modern_javascript_exercises/imperative_vs_declarative/imperative_vs_declarative.js
@@ -1,10 +1,5 @@
 // Instructions can be found in imperative_vs_declarative.md
 
 export function longPasswords(passwords) {
-  let longPasswords = [];
-  passwords.forEach(element => { if (element.length >= 9 ) 
-    longPasswords.push(element);
-    
-  });
-  return longPasswords;
+  return passwords.filter(element => (element.length >= 9 )); 
 }
