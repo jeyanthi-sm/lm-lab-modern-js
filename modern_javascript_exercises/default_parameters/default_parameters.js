1d814ce846d826060d9b5d993bb7cbd136773fad
jeyanthi-sm <111299753+jeyanthi-sm@users.noreply.github.com>
2022-12-19 21:06:25 +0000

    updated to use function default parameter instead using of ternary operator to check the value

--- modern_javascript_exercises/default_parameters/default_parameters.js
+++ modern_javascript_exercises/default_parameters/default_parameters.js
@@ -1,6 +1,5 @@
 // Instructions can be found in default_parameters.md
 
-export const sayHello = (person) => {
-  return person ? "Hello, " + person+"!" :"Hello, you!"; 
- // return `Hello, ${person === undefined ? "you!" : person + "!"}`;
+export const sayHello = (person="you") => {
+  return "Hello, " + person+"!";
 };
