b66f5ab860934562a231850008c3e761647a50d8
jeyanthi-sm <111299753+jeyanthi-sm@users.noreply.github.com>
2023-01-10 05:38:07 +0000

    destructuruing_arrays.js and destructuring_arrays.test.js files are removed

--- modern_javascript_exercises/destructuring_arrays/destructuring_arrays.test.js
+++
@@ -1,6 +0,0 @@
-import { sequence } from "./destructuring_arrays";
-
-test("it returns the correct order from an array", () => {
-  const order = ["First", "Second", "Third"];
-  expect(sequence(order)).toBe("First, Second, Third");
-});
